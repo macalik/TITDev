1e664513d60521caa4613ddc19ae123679ec2054
SnipersCode <validtechrange@gmail.com>
2015-08-30 19:39:49 -0500

    Basic placeholder site
    Placeholder until something is implemented.

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+import navigation
 
 app = Flask(__name__)
+Bootstrap(app)
+
+navigation.init(app)
 
 
 @app.route('/')
 def hello():
-    return "TiT website coming soon!"
+    return render_template("index.html", name="hi")
+
+
+@app.route('/something')
+def other():
+    return render_template("base.html", name="hello")
+
+if "HEROKU" not in os.environ and __name__ == "__main__":
+    app.debug = True
+    app.run()
